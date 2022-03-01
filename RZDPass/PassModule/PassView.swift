fb7babf65bee55ed06927f44dccdc8a52538ae65
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-03-01 11:54:54 +0300

    Create pass module

---
+++ RZDPass/PassModule/PassView.swift
@@ -0,0 +1,20 @@
+//
+//  PassView.swift
+//  RZDPass
+//
+//  Created by tambanco 🥳 on 01.03.2022.
+//
+
+import UIKit
+
+class PassView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ RZDPass/PassModule/PassViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PassViewController.swift
+//  RZDPass
+//
+//  Created by tambanco 🥳 on 01.03.2022.
+//
+
+import UIKit
+
+class PassViewController: UIView {
+
+}
