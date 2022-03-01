53c60a26dc74e034efb1f01a8c74ad3bcc0632e8
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-03-01 14:54:31 +0300

    Update PassView.swift
    Add background figure

--- RZDPass/PassModule/PassView.swift
+++ RZDPass/PassModule/PassView.swift
@@ -22,13 +22,18 @@ class PassView: UIView {
         passShapeLayer.frame = bounds
         
         let path = UIBezierPath()
-        path.move(to: CGPoint(x: 30, y: 200))
+        path.move(to: CGPoint(x: 30, y: 210))
+        path.addArc(withCenter: CGPoint(x: 40, y: 210), radius: 10, startAngle: Double.pi, endAngle: (Double.pi*3)/2, clockwise: true)
         path.addLine(to: CGPoint(x: 300, y: 200))
         path.addArc(withCenter: CGPoint(x: 300, y: 210), radius: 10, startAngle: Double.pi/2, endAngle: 0, clockwise: true)
         path.addLine(to: CGPoint(x: 310, y: 350))
         path.addArc(withCenter: CGPoint(x: 310, y: 360), radius: 10, startAngle: (Double.pi*3)/2, endAngle: Double.pi/2, clockwise: false)
         path.addLine(to: CGPoint(x: 310, y: 600))
-        path.addArc(withCenter: CGPoint(x: 300, y: 600), radius: 10, startAngle: Double.pi, endAngle: Double.pi/2, clockwise: true)
+        path.addArc(withCenter: CGPoint(x: 300, y: 590), radius: 10, startAngle: 0, endAngle: Double.pi/2, clockwise: true)
+        path.addLine(to: CGPoint(x: 40, y: 600))
+        path.addArc(withCenter: CGPoint(x: 40, y: 590), radius: 10, startAngle: Double.pi/2, endAngle: Double.pi, clockwise: true)
+        path.addLine(to: CGPoint(x: 30, y: 360))
+        path.addArc(withCenter: CGPoint(x: 30, y: 360), radius: 10, startAngle: Double.pi/2, endAngle: (Double.pi*3)/2, clockwise: false)
             
         passShapeLayer.path = path.cgPath
         passShapeLayer.fillColor = UIColor(rgb: 0x75140D).cgColor
