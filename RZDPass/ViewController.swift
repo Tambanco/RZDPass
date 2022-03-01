a2a515d71da090880e8645960ce751aaf0af36bf
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-03-01 11:52:49 +0300

    add Project files

---
+++ RZDPass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RZDPass
+//
+//  Created by tambanco 🥳 on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
