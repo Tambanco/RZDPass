d22fa7f550e66758a0ee4b130e9cf9adc1daabe5
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-03-01 12:24:01 +0300

    Update PassViewController

--- RZDPass/PassModule/PassView.swift
+++ RZDPass/PassModule/PassView.swift
@@ -9,6 +9,37 @@ import UIKit
 
 class PassView: UIView {
 
+    // MARK: - 1 layer
+    let passShapeLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.backgroundColor = UIColor(rgb: 0x75140D).cgColor
+        return layer
+    }()
     
-
+    override init(frame: CGRect) {
+        super.init(frame: CGRect.zero)
+        
+        backgroundColor = .red
+        
+        layer.addSublayer(passShapeLayer)
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 20, y: 200))
+        path.addLine(to: CGPoint(x: 400, y: 200))
+        path.addArc(withCenter: CGPoint(x: 400, y: 220),
+                    radius: Double.pi,
+                    startAngle: Double.pi/2,
+                    endAngle: 0,
+                    clockwise: true)
+        path.addLine(to: CGPoint(x: 400, y: 400))
+        path.addArc(withCenter: CGPoint(x: 400, y: 10),
+                    radius: Double.pi/2,
+                    startAngle: Double.pi/2,
+                    endAngle: Double.pi*2,
+                    clockwise: false)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- RZDPass/PassModule/PassViewController.swift
+++ RZDPass/PassModule/PassViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class PassViewController: UIViewController {
+    
+    var passView: PassView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        passViewBuilder()
+    }
+    
+    func passViewBuilder() {
+        passView = PassView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+        view.addSubview(passView)
     }
 
 }
